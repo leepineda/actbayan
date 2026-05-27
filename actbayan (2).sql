3114ea4b862c9696e1fd28e4868f29f955236c56
freddo26 <168149090+freddo26@users.noreply.github.com>
2026-05-27 11:03:42 +0800

    Add files via upload

---
+++ actbayan (2).sql
@@ -0,0 +1,589 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 27, 2026 at 05:00 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `actbayan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `account_id` int(11) NOT NULL,
+  `first_name` varchar(50) NOT NULL,
+  `last_name` varchar(100) NOT NULL,
+  `middle_name` varchar(50) NOT NULL,
+  `dob` date NOT NULL,
+  `profile_photo` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `accounts`
+--
+
+INSERT INTO `accounts` (`account_id`, `first_name`, `last_name`, `middle_name`, `dob`, `profile_photo`) VALUES
+(1, 'Fred', 'Rick', 'Saging', '2026-04-12', ''),
+(2, 'Juswa', 'VHAAS', 'fsf', '2026-05-26', ''),
+(3, 'Lee', 'GO', 'dd', '2005-02-12', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `activity_logs`
+--
+
+CREATE TABLE `activity_logs` (
+  `al_id` int(11) NOT NULL,
+  `report_id` int(11) DEFAULT NULL,
+  `action` varchar(255) DEFAULT NULL,
+  `made_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `announcements`
+--
+
+CREATE TABLE `announcements` (
+  `ann_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `ann_title` varchar(254) DEFAULT NULL,
+  `ann_desc` text DEFAULT NULL,
+  `submitted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `contact_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `phone_number` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`contact_id`, `account_id`, `email`, `phone_number`) VALUES
+(1, 1, 'sanfrbs26@gmail.com', NULL),
+(2, 2, NULL, '09280503740'),
+(3, 3, 'rere@gmail.com', NULL);
+
+--
+-- Triggers `contacts`
+--
+DELIMITER $$
+CREATE TRIGGER `passcontact` AFTER INSERT ON `contacts` FOR EACH ROW BEGIN
+    IF NEW.email IS NOT NULL THEN
+        INSERT INTO usercreds(contact_id, emorph)
+        VALUES (NEW.contact_id, NEW.email);
+    END IF;
+
+    IF NEW.phone_number IS NOT NULL THEN
+        INSERT INTO usercreds(contact_id, emorph)
+        VALUES (NEW.contact_id, NEW.phone_number);
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credentials`
+--
+
+CREATE TABLE `credentials` (
+  `cred_id` int(11) NOT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `pass_id` int(11) DEFAULT NULL,
+  `failed_attempts` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `credentials`
+--
+
+INSERT INTO `credentials` (`cred_id`, `user_id`, `pass_id`, `failed_attempts`) VALUES
+(1, 1, 1, NULL),
+(2, 2, 2, NULL),
+(3, 3, 3, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedbacks`
+--
+
+CREATE TABLE `feedbacks` (
+  `feedback_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `report_id` int(11) DEFAULT NULL,
+  `feedback` text DEFAULT NULL,
+  `uploaded_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `feedbacks`
+--
+
+INSERT INTO `feedbacks` (`feedback_id`, `account_id`, `report_id`, `feedback`, `uploaded_at`) VALUES
+(1, 1, 1, 'pass po', '2026-05-27 10:58:32');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `location`
+--
+
+CREATE TABLE `location` (
+  `location_id` int(11) NOT NULL,
+  `locname` varchar(254) DEFAULT NULL,
+  `x` decimal(10,7) NOT NULL,
+  `y` decimal(10,7) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `location`
+--
+
+INSERT INTO `location` (`location_id`, `locname`, `x`, `y`) VALUES
+(4, 'Llanera, San Francisco, Nueva Ecija', 15.7098830, 121.0094470);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `password`
+--
+
+CREATE TABLE `password` (
+  `pass_id` int(11) NOT NULL,
+  `password` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `password`
+--
+
+INSERT INTO `password` (`pass_id`, `password`) VALUES
+(1, 'lolo'),
+(2, 'haha'),
+(3, 'lolop');
+
+--
+-- Triggers `password`
+--
+DELIMITER $$
+CREATE TRIGGER `passpass` AFTER INSERT ON `password` FOR EACH ROW BEGIN
+	UPDATE credentials
+	SET pass_id = NEW.pass_id
+	WHERE cred_id = (SELECT MAX(cred_id) FROM credentials); 
+end
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registration`
+--
+
+CREATE TABLE `registration` (
+  `registration_id` int(11) NOT NULL,
+  `email_address` varchar(50) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `phone_number` varchar(11) NOT NULL,
+  `created_at` date NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reports`
+--
+
+CREATE TABLE `reports` (
+  `report_id` int(11) NOT NULL,
+  `account_id` int(11) NOT NULL,
+  `location_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'Pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `verified_at` timestamp NULL DEFAULT NULL,
+  `verified_by` int(11) DEFAULT NULL,
+  `category` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `reports`
+--
+
+INSERT INTO `reports` (`report_id`, `account_id`, `location_id`, `title`, `image_url`, `description`, `status`, `created_at`, `verified_at`, `verified_by`, `category`) VALUES
+(1, 1, 4, 'hjhjgjh', 'static/uploads/concerns/concern_1_1779240893_Screenshot (17).png', 'xcggfddgs', 'Pending', '2026-05-20 01:34:53', NULL, NULL, 'Flooding');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `report_update`
+--
+
+CREATE TABLE `report_update` (
+  `reportupd_id` int(11) NOT NULL,
+  `account_id` int(11) DEFAULT NULL,
+  `report_id` int(11) DEFAULT NULL,
+  `image_url` text DEFAULT NULL,
+  `description` text DEFAULT NULL,
+  `submitted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `report_update`
+--
+
+INSERT INTO `report_update` (`reportupd_id`, `account_id`, `report_id`, `image_url`, `description`, `submitted_at`) VALUES
+(1, 2, 1, 'static/uploads/update_1_1779241047_Screenshot (51).png', 'okay na po', '2026-05-20 09:37:27');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `role`
+--
+
+CREATE TABLE `role` (
+  `role_id` int(11) NOT NULL,
+  `role` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`role_id`, `role`) VALUES
+(1, 'Resident'),
+(2, 'LGU Official'),
+(3, 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usercreds`
+--
+
+CREATE TABLE `usercreds` (
+  `user_id` int(11) NOT NULL,
+  `contact_id` int(11) DEFAULT NULL,
+  `emorph` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `usercreds`
+--
+
+INSERT INTO `usercreds` (`user_id`, `contact_id`, `emorph`) VALUES
+(1, 1, 'sanfrbs26@gmail.com'),
+(2, 2, '09280503740'),
+(3, 3, 'rere@gmail.com');
+
+--
+-- Triggers `usercreds`
+--
+DELIMITER $$
+CREATE TRIGGER `insertuser` AFTER INSERT ON `usercreds` FOR EACH ROW BEGIN
+	INSERT INTO credentials (user_id) VALUES (NEW.user_id);
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE `user_role` (
+  `user_id` int(11) DEFAULT NULL,
+  `role_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user_role`
+--
+
+INSERT INTO `user_role` (`user_id`, `role_id`) VALUES
+(1, 1),
+(2, 2),
+(3, 3);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`account_id`);
+
+--
+-- Indexes for table `activity_logs`
+--
+ALTER TABLE `activity_logs`
+  ADD PRIMARY KEY (`al_id`),
+  ADD KEY `report_id` (`report_id`);
+
+--
+-- Indexes for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD PRIMARY KEY (`ann_id`),
+  ADD UNIQUE KEY `ann_title` (`ann_title`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`contact_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD PRIMARY KEY (`cred_id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `pass_id` (`pass_id`);
+
+--
+-- Indexes for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD PRIMARY KEY (`feedback_id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `report_id` (`report_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`);
+
+--
+-- Indexes for table `password`
+--
+ALTER TABLE `password`
+  ADD PRIMARY KEY (`pass_id`);
+
+--
+-- Indexes for table `registration`
+--
+ALTER TABLE `registration`
+  ADD PRIMARY KEY (`registration_id`),
+  ADD UNIQUE KEY `email_address` (`email_address`);
+
+--
+-- Indexes for table `reports`
+--
+ALTER TABLE `reports`
+  ADD PRIMARY KEY (`report_id`),
+  ADD KEY `location_id` (`location_id`),
+  ADD KEY `account_id` (`account_id`);
+
+--
+-- Indexes for table `report_update`
+--
+ALTER TABLE `report_update`
+  ADD PRIMARY KEY (`reportupd_id`),
+  ADD KEY `account_id` (`account_id`),
+  ADD KEY `report_id` (`report_id`);
+
+--
+-- Indexes for table `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`role_id`);
+
+--
+-- Indexes for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  ADD PRIMARY KEY (`user_id`),
+  ADD KEY `contact_id` (`contact_id`);
+
+--
+-- Indexes for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `role_id` (`role_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `activity_logs`
+--
+ALTER TABLE `activity_logs`
+  MODIFY `al_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `announcements`
+--
+ALTER TABLE `announcements`
+  MODIFY `ann_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `contact_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `credentials`
+--
+ALTER TABLE `credentials`
+  MODIFY `cred_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  MODIFY `feedback_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `password`
+--
+ALTER TABLE `password`
+  MODIFY `pass_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `registration`
+--
+ALTER TABLE `registration`
+  MODIFY `registration_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `reports`
+--
+ALTER TABLE `reports`
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `report_update`
+--
+ALTER TABLE `report_update`
+  MODIFY `reportupd_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `role`
+--
+ALTER TABLE `role`
+  MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `activity_logs`
+--
+ALTER TABLE `activity_logs`
+  ADD CONSTRAINT `activity_logs_ibfk_1` FOREIGN KEY (`report_id`) REFERENCES `reports` (`report_id`);
+
+--
+-- Constraints for table `announcements`
+--
+ALTER TABLE `announcements`
+  ADD CONSTRAINT `announcements_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+
+--
+-- Constraints for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD CONSTRAINT `contacts_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+
+--
+-- Constraints for table `credentials`
+--
+ALTER TABLE `credentials`
+  ADD CONSTRAINT `credentials_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usercreds` (`user_id`),
+  ADD CONSTRAINT `credentials_ibfk_2` FOREIGN KEY (`pass_id`) REFERENCES `password` (`pass_id`);
+
+--
+-- Constraints for table `feedbacks`
+--
+ALTER TABLE `feedbacks`
+  ADD CONSTRAINT `feedbacks_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
+  ADD CONSTRAINT `feedbacks_ibfk_2` FOREIGN KEY (`report_id`) REFERENCES `reports` (`report_id`);
+
+--
+-- Constraints for table `reports`
+--
+ALTER TABLE `reports`
+  ADD CONSTRAINT `reports_ibfk_1` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`),
+  ADD CONSTRAINT `reports_ibfk_2` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`);
+
+--
+-- Constraints for table `report_update`
+--
+ALTER TABLE `report_update`
+  ADD CONSTRAINT `report_update_ibfk_1` FOREIGN KEY (`account_id`) REFERENCES `accounts` (`account_id`),
+  ADD CONSTRAINT `report_update_ibfk_2` FOREIGN KEY (`report_id`) REFERENCES `reports` (`report_id`);
+
+--
+-- Constraints for table `usercreds`
+--
+ALTER TABLE `usercreds`
+  ADD CONSTRAINT `usercreds_ibfk_1` FOREIGN KEY (`contact_id`) REFERENCES `contacts` (`contact_id`);
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `usercreds` (`user_id`),
+  ADD CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`role_id`) REFERENCES `role` (`role_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
